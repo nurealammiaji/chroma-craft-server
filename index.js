beb07b86cba28246a48a4158a9282f2da3b250ac
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-02 02:22:19 +0600

    mongodb driver added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express();
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
 
 app.get("/", (req, res) => {
     res.send("Chroma Craft Server");
@@ -17,3 +20,29 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Chroma Craft Server is running on port: ${port}`);
 })
+
+// MongoDB Driver
+const uri = `mongodb+srv://${username}:${password}@cluster0.31s3qjy.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
