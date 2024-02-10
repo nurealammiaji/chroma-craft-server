fb7011e9e11561b106ca9d2714f321ea0993bcea
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-10 17:46:07 +0600

    instructors getting api added

--- index.js
+++ index.js
@@ -58,6 +58,13 @@ async function run() {
             res.send(result);
         })
 
+        // Instructors API
+        app.get("/instructors", async (req, res) => {
+            const cursor = instructorCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
