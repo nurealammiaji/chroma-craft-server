86580c780dacd4e36b96f3301498c0b86a4d360c
nurealammiaji <nurealam.miaji@gmail.com>
2024-02-14 01:41:34 +0600

    user adding api added

--- index.js
+++ index.js
@@ -72,6 +72,12 @@ async function run() {
             res.send(result);
         })
 
+        app.post("/users", async (req, res) => {
+            const user = req.body.user;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
